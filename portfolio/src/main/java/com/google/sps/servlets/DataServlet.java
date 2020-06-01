0bf6048a7a5f62c935ecc0992725beb89ccb4412
ataddesse <amanueltesfaye44@gmail.com>
2020-06-01 13:14:23 -0400

    Step 4 of week 3. Adding a comment feature.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -48,4 +48,26 @@ public class DataServlet extends HttpServlet {
     response.getWriter().println(json);
     
   }
+
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // If the user sends another POST request after the game is over, then start a new game.
+    if (game.isGameOver()) {
+      game = new SubtractionGame();
+    }
+
+    // Get the input from the form.
+    int playerChoice = getPlayerChoice(request);
+    if (playerChoice == -1) {
+      response.setContentType("text/html");
+      response.getWriter().println("Please enter an integer between 1 and 3.");
+      return;
+    }
+
+    game.takePlayerTurn(playerChoice);
+
+    // Redirect back to the HTML page.
+    response.sendRedirect("/index.html");
+  }
+
 }
