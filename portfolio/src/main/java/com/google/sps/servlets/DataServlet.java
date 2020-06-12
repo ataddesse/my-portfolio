7c37937e6db9303864dbe6d109dcddb30e66fdc5
ataddesse <amanueltesfaye44@gmail.com>
2020-06-12 20:08:49 +0000

    First commit to add the Sentiment analysis feature

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,7 +14,9 @@
 
 package com.google.sps.servlets;
 
-
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -37,7 +39,6 @@ public class DataServlet extends HttpServlet {
  
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-   
     long timestamp = System.currentTimeMillis();
     
     //Using the fetched variables with the taskEntity
@@ -47,6 +48,17 @@ public class DataServlet extends HttpServlet {
     taskEntity.setProperty("email", request.getParameter("email"));
     taskEntity.setProperty("comment", request.getParameter("comment"));
     taskEntity.setProperty("timestamp", timestamp);
+
+    String message = request.getParameter("comment");
+
+    Document doc =
+        Document.newBuilder().setContent(message).setType(Document.Type.PLAIN_TEXT).build();
+    LanguageServiceClient languageService = LanguageServiceClient.create();
+    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
+    float score = sentiment.getScore();
+    languageService.close();
+
+    taskEntity.setProperty("score", score);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(taskEntity);
